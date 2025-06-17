ae034d4fdd40efae1f1ab758593982e51d9d354f
Yash Sahay <dive.with.yash@gmail.com>
2025-06-17 20:41:34 +0530

    First Commit

---
+++ app/lg/page.tsx
@@ -0,0 +1,13 @@
+export default function LgPage() {
+  return (
+    <html>
+      <head>
+        <title>LG Page</title>
+      </head>
+      <body>
+        <h1>Welcome to the LG Page</h1>
+        <p>This is a basic HTML page in the <code>lg</code> folder.</p>
+      </body>
+    </html>
+  );
+}
